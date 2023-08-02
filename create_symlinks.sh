3f88b60f687bb9cf16622007cda4b1d47314b006
amantri <829820+amantri@users.noreply.github.com>
2023-08-01 22:02:45 -0700

    Add .tmux.conf

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -6,7 +6,7 @@
 SOURCE="/Users/amantri"
 DEST="/Users/amantri/gDrive/code/dotfiles_mac"
 
-for file in .bash_aliases .bash_profile .bashrc .fzf.bash .fzf.zsh .gitconfig .vimrc .zprofile .zshrc
+for file in .bash_aliases .bash_profile .bashrc .fzf.bash .fzf.zsh .gitconfig .tmux.conf .vimrc .zprofile .zshrc
 do
   echo; echo $file
   mv $SOURCE/$file $DEST/$file
